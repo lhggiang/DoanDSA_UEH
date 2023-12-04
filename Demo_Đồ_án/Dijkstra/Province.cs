d6afb5c6c1fa33c3a2aa4a1d6a85d784d4f6ffe7
= <john@example.com>
2023-12-04 21:42:32 +0700

    Nhom 9

---
+++ Demo_Đồ_án/Dijkstra/Province.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    public class Province
+    {
+        public string provinceName { get; set; }
+        public string verticesName { get; set; }
+        public Point verticesLocation { get; set; }
+        public Province(string provinceName, string verticesName, int x, int y)
+        {
+            this.provinceName = provinceName;
+            this.verticesName = verticesName;
+            Point P = new Point(x, y);
+            this.verticesLocation = P;
+
+        }
+        public string getName()
+        {
+            return provinceName;
+        }
+        public string getVerticesName()
+        {
+            return verticesName;
+        }
+        public Point getPoint()
+        {
+            return verticesLocation;
+        }
+    }
+}
---
+++ Demo_Đồ_án/Dijkstra/VerticesProvince.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    public class Vertex
+    {
+        public string nameVertices { get; private set; }
+        public bool wasVisited { get; set; }
+        public string previousVertices { get; set; }
+        public double pathLenght { get; set; }
+        public Vertex(string s)
+        {
+            this.nameVertices = s;
+        }
+    }
+}
